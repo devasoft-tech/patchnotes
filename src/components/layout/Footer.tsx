249bd39a19fde6ab4e2a9712e65d1066de59a500
devasoft-tech <devasoftworldwide@gmail.com>
2025-05-22 20:31:11 +0530

    update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
       <div className="container flex flex-col items-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by{" "}
+            {/* Built by{" "}
             <a
               href="https://twitter.com/yourusername"
               target="_blank"
@@ -18,7 +18,8 @@ export function Footer() {
             >
               your-name
             </a>
-            . Made for devs who hate noise.
+            .  */}
+            Made for devs who hate noise.
           </p>
         </div>
         <div className="flex flex-1 items-center justify-end gap-4">
